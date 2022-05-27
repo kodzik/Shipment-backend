968d02596b24e746a460becc53c3b0044d1e2586
kodzik <pawlopoczta@wp.pl>
2022-05-27 09:33:44 +0200

    Minor fixes.

--- shipments/shipments/urls.py
+++ shipments/shipments/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('main.urls')),
+    path('', TemplateView.as_view(template_name="index.html"), name='home'),
 ]
