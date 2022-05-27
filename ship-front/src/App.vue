bb7e08dd20b85eef5afa2c800c3e73b68eb40a17
kodzik <pawlopoczta@wp.pl>
2022-05-27 12:12:31 +0200

    Minor changes.

--- ship-front/src/App.vue
+++ ship-front/src/App.vue
@@ -1,18 +1,3 @@
-<script setup lang="ts">
-  export interface IShipment {
-    shipment_id: number
-    order_id: number 
-    ship_date: Date 
-    origin: string
-    destination: string
-    customer: string
-    type: string
-    carrier: string
-    status: string
-  }
-</script>
-
 <template>
   <RouterView />
 </template>
-
