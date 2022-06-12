b790233314800d685ec78ff9c2a8738e724e3c23
pawel <plegiec@elmi.pl>
2022-06-12 16:26:43 +0200

    Fixed CORS for dev.

--- shipments/shipments/settings.py
+++ shipments/shipments/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
 ]
 # Application definition
 
